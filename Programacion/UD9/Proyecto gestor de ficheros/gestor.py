e0417e5b30aec4cad7912514d0aa9f1e2e0f44f9
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-03-13 19:44:39 +0100

    sa

--- Programacion/UD9/Proyecto gestor de ficheros/gestor.py
+++ Programacion/UD9/Proyecto gestor de ficheros/gestor.py
@@ -5,22 +5,26 @@ def crear_usuario():
     contra = input("Indique una contraseña: ")
 
     with open("usuarios.csv", "a") as newuser:
-        newuser.write(nombre + "," + contra)
+        newuser.write(f"{nombre},{contra}\n")
+    print("¡Usuario creado!")
 
 def validar_usuario():
-    nombre=input("Nombre: ")
-    contraseña=input("Contraseña: ")
+    nombre = input("Nombre: ")
+    contraseña = input("Contraseña: ")
+
     with open("usuarios.csv", "r") as user:
-        for i in user:
-            nc = i.split(",")
-            if nombre == nc[0] and contraseña == nc[1]:
-                print(f"¡Bienvenido {nc[0]}!")
+        for linea in user:
+            datos = linea.strip().split(",")
+
+            if nombre == datos[0] and contraseña == datos[1]:
+                print(f"¡Bienvenido {datos[0]}!\n")
                 return True
-            else:
-                print("Usuario y contraseña erroneos.")
-                return False
+
+    print("Usuario y contraseña erróneos.")
+    return False
 
 def organizar_archivos(archivo, extension):
+    
     if extension == "xls":
         carpeta = "Facturas"
     elif extension == "docx":
@@ -43,7 +47,8 @@ def organizar_archivos(archivo, extension):
 def menu_usuario():
     opciones_usuario = [
         "Iniciar Sesion",
-        "Crear usuario"
+        "Crear Usuario",
+        "Cerrar Programa"
     ]
     for i,o in enumerate(opciones_usuario):
         print(f"{i}: {o}")
@@ -56,28 +61,32 @@ def menu_sistema():
     for i,o in enumerate(opciones_sistema):
         print(f"Presiona {i} para: {o}")
 
-lista_archivos = os.listdir(".")
-
-for i in lista_archivos:
-    ip = i.split(".")
-    if len(ip) > 1:
-        if ip[0] != "log" and ip[-1] != "py":
-            organizar_archivos(i,ip[-1])
 salir=False
+validado=False
 
 while not salir:
 
     menu_usuario()
-    opcion_usuario=int(input("Bienvenido que desea hacer?\n"))
-
-    match opcion_usuario:
-
-        case 0:
-            validado = validar_usuario()
-        case 1:
-            crear_usuario()
+    while True:
+        opcion_usuario=int(input("Bienvenido que desea hacer?\n"))
+    
+        match opcion_usuario:
+            case 0:
+                validado = validar_usuario()
+                break
+            case 1:
+                crear_usuario()
+                break
+            case 2:
+                break
 
     while validado:
 
         menu_sistema()
         opcion_sistema = input("Indique una opcion:\n")
+        lista_archivos = os.listdir(".")
+        for i in lista_archivos:
+            ip = i.split(".")
+            if len(ip) > 1:
+                if ip[0] != "log" and ip[-1] != "py":
+                    organizar_archivos(i,ip[-1])
