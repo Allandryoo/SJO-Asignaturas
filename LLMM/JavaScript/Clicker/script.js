9bf71ebc4a8c5ae32d0ccaba3d78d8a57d5cc470
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-03-31 20:28:16 +0200

    31/3/2026 - 20:28

--- LLMM/JavaScript/Clicker/script.js
+++ LLMM/JavaScript/Clicker/script.js
@@ -1,13 +1,37 @@
-const countElement = document.getElementById('count');
-const clickBtn = document.getElementById('click-btn');
-const lvlUpBtn = document.getElementById('reset-btn');
-//const lvlUpBtnPlus =
-let count = 0;
+const contador = document.querySelector("#contador");
+const clickBtn = document.querySelector("#botonClicker");
+const textMejora = document.querySelector(".mejoras h3")
+const lvlUpBtn1 = document.querySelector("#mejora1");
+const lvlUpBtn2 = document.querySelector("#mejora2");
+const mensajeFinal = document.querySelector("#mensajeFinal")
 
-clickBtn.addEventListener('click', () => {
-    count++;
+let count = 0;
+let multi = 100;
+
+clickBtn.addEventListener("click", () => {
+    count += multi;
+    contador.textContent = count;
+    if (count >= 10000) {
+        clickBtn.style.display = "none";
+        lvlUpBtn1.style.display = "none";
+        lvlUpBtn2.style.display = "none";
+        textMejora.style.display = "none"
+        mensajeFinal.textContent = "¡Felicidades! Has llegado al máximo.";
+    }
 });
 
-resetBtn.addEventListener('click', () => {
-    count = 0;
+lvlUpBtn1.addEventListener("click", () => {
+    if (count >= 200) {
+        count -= 200;
+        multi *= 2;
+        contador.textContent = count;
+    }
+});
+
+lvlUpBtn2.addEventListener("click", () => {
+    if (count >= 1000) {
+        count -= 1000;
+        multi *= 4;
+        contador.textContent = count;
+    }
 });
