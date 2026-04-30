659603564c65c9c329df58b2455898808377553f
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-04-30 18:06:04 +0200

    30/04/2026 18:05

--- LLMM/JSON/Ejercicio2/script.js
+++ LLMM/JSON/Ejercicio2/script.js
@@ -1,22 +1,30 @@
 let alumnos = [];
+const contenedorAlumnos = document.getElementById("tabla-alumnos")
 
 async function cargarAlumnos() {
     const response = await fetch("alumnos.json")
     const datos = await response.json();
-
-    const contenedorAlumnos = document.querySelector("#tabla-alumnos")
+    console.log(datos)
+    datos.alumnos.forEach(alumno => {
+        alumnos +=
+            `
+            <tr>    
+                <td>${alumno.nombre}</td>
+                <td>${alumno.edad}</td>
+                <td>${alumno.curso}</td>
+                <td>${alumno.notaMedia}</td>
+            </tr>
+            `
+    });
+    contenedorAlumnos.innerHTML = alumnos;
 }
-
-datos.alumnos.array.forEach(alumno => {
-    alumnos +=
-});
 
 function mostrarAlumnos(lista) {
 
 }
 
 document.getElementById("btnTodos").addEventListener("click", () => {
-
+    cargarAlumnos()
 });
 
 document.getElementById("btnAprobados").addEventListener("click", () => {
