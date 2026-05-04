c67155e48fdee5c51834831b22c1ac76eff4ffc6
Allandryoo <asantosd@alumnes.santjosepobrer.es>
2026-05-04 13:58:32 +0200

    avance inicio y registro de spotify

--- LLMM/Spotify/scripts/verificacion.js
+++ LLMM/Spotify/scripts/verificacion.js
@@ -1,4 +1,6 @@
 const btnIniciarSesion = document.querySelector("#btnIniciarSesion");
+const inicioSesion = document.querySelector(".inicio-sesion");
+const registrar = document.querySelector(".registrar");
 
 btnIniciarSesion.addEventListener("click", () => {
     
